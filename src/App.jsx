87e362f1ceca2a833ced8cd75a60baf77380f874
Pawan Kumar <pyadav.workmail@gmail.com>
2024-03-17 11:43:35 +0530

    about section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import About from './components/About'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import Services from './components/Services'
@@ -12,6 +13,7 @@ function App() {
       <Navbar/>
       <Home/>
       <Services/>
+      <About/>
     </>
   )
 }
